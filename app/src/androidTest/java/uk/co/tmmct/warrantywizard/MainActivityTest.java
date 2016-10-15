fe59b503a94fbb475e1899fa7e4d071d37696fe6
Tom McTiernan <thomasmctiernan@gmail.com>
2016-10-15 15:19:37 +0100

    Test

--- app/src/androidTest/java/uk/co/tmmct/warrantywizard/MainActivityTest.java
+++ app/src/androidTest/java/uk/co/tmmct/warrantywizard/MainActivityTest.java
@@ -1,7 +1,5 @@
 package uk.co.tmmct.warrantywizard;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,7 +11,6 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
